640a3775619b068dac8e0ff9b4d58c53ba6c6308
Yan Furman <furmanyan@gmail.com>
2020-06-20 18:19:30 +0400

    Realised method setting coordinates of interest area

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -37,10 +38,25 @@ namespace attention_control
 
         private void btnManualCoords_Click(object sender, RoutedEventArgs e)
         {
-            //если координаты введены верно
-                //задать координаты
+            //если в поля координат введены неотрицательные целые числа
+            if (Regex.IsMatch(boxX1.Text, "^\\d+") && Regex.IsMatch(boxX2.Text, "^\\d+") && Regex.IsMatch(boxY1.Text, "^\\d+") && Regex.IsMatch(boxY2.Text, "^\\d+"))
+            {
+                tmp_leftUpX = Convert.ToInt32(boxX1.Text); tmp_leftUpY = Convert.ToInt32(boxY1.Text); tmp_rightDownX = Convert.ToInt32(boxX2.Text); tmp_rightDownY = Convert.ToInt32(boxY2.Text);
+                //если координаты введены верно
+                if (!(tmp_leftUpX >= tmp_rightDownX || tmp_leftUpY >= tmp_rightDownY))
+                {
+                    //задать координаты
+                    leftUpX = tmp_leftUpX; leftUpY = tmp_leftUpY; rightDownX = tmp_rightDownX; rightDownY = tmp_rightDownY;
+                    labelAreaCoordinates.Text = "Установлены координаты:\nX1 = " + leftUpX + "\nY1 = " + leftUpY + "\nX2 = " + rightDownX + "\nY2 = " + rightDownY;
+                }
+            }
             //иначе
+            else
+            {
                 //сообщить, что координаты введены неверно
+                labelAreaCoordinates.Text = "Координаты введены неверно";
+                leftUpX = -1; leftUpY = -1; rightDownX = -1; rightDownY = -1;
+            }
         }
 
         private void btnStartStop_Click(object sender, RoutedEventArgs e)
